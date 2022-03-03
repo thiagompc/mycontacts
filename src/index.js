0161601064432370ec819ed136e5c0a89ab02b23
Thiago <thiago099carvalho@gmail.com>
2022-03-03 02:14:02 -0300

    add eslint and editorconfig

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-    response.send('Hello world!');
+  response.send('Hello world!');
 });
 
 app.listen(2000, () => console.log('Server started at http://localhost:2000'));
