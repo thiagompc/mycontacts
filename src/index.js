9880612b7b0aab5854eff7d7993374b40cfd675b
Thiago <thiago099carvalho@gmail.com>
2022-03-03 01:19:05 -0300

    inicialize express app

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (request, response) => {
+    response.send('Hello world!');
+});
+
+app.listen(2000, () => console.log('Server started at http://localhost:2000'));
