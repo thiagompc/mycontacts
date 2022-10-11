c40425f4360dcd365dc329cbce6e4dc78a91c21a
Thiago <thiago099carvalho@gmail.com>
2022-10-11 19:44:27 -0300

    create Contacts repository

---
+++ src/app/repositories/ContactsRepository.js
@@ -0,0 +1,21 @@
+const { uuid } = require('uuidv4');
+
+const contacts = [
+  {
+    id: uuid(),
+    name: 'Mateus',
+    email: 'mateus@mail.com',
+    phone: '123123213',
+    category_id: uuid(),
+  },
+];
+
+class ContactsRepository {
+  findAll() {
+    return new Promise((resolve) => {
+      resolve(contacts);
+    });
+  }
+}
+
+module.exports = new ContactsRepository();
